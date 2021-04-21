deb03f3a274defea819d04fad8e7df8b981f668a
Hugo Medina <hhmedinaguzman@gmail.com>
2021-04-20 23:49:04 -0500

    Proyecto Bootstrap terminado

---
+++ index.js
@@ -0,0 +1,23 @@
+/*const button1 = document.getElementById("bcard-1");
+const button2 = document.getElementById("bcard-2");
+const button3 = document.getElementById("bcard-3");
+
+
+button1.addEventListener('click',()=>{
+    let modElement = document.getElementById("mod1");
+    let mod = new bootstrap.Modal(modElement);
+    mod.show();
+});
+
+button2.addEventListener('click',()=>{
+    let modElement = document.getElementById("mod2");
+    let mod = new bootstrap.Modal(modElement);
+    mod.show();
+});
+
+button3.addEventListener('click',()=>{
+    let modElement = document.getElementById("mod3");
+    let mod = new bootstrap.Modal(modElement);
+    mod.show();
+});
+*/
